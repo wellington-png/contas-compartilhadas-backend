d67d0f8fff4388990a6773e52ef0be6c35ec282c
Wellington Santos <wellingtonasc38@gmail.com>
2024-09-02 16:02:47 -0300

    expense

--- apps/finances/serializers/expense.py
+++ apps/finances/serializers/expense.py
@@ -1,11 +1,20 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from apps.finances.models import Expense
 
 
 class ExpenseSerializer(ModelSerializer):
+    name_user = serializers.CharField(source="user.email", read_only=True)
     class Meta:
         model = Expense
-        fields = ["id", "amount", "description", "date_spent", "group", "is_fixed"]
-        read_only_fields = ["id"]
-
-
+        fields = [
+            "id",
+            "amount",
+            "description",
+            "date_spent",
+            "group",
+            "is_fixed",
+            "user",
+            "name_user",
+        ]
+        read_only_fields = ["id", "user"]
